f1bc1f172804611ec67a89629740d3bad923dc58
mitulg1995 <68859472+mitulg1995@users.noreply.github.com>
2020-07-31 15:09:48 -0400

    Add files via upload
    Try to copy and paste some paragraph in this code. It can handle that.
    
    Improvements from 2 -> 2_m2 :-
    • Space character did not give the final pyramid a good look so it will filter all the spaces.
    • Will provide some more information than previous version

---
+++ 2/2_m2.py
@@ -0,0 +1,27 @@
+# Code to count Occurrences in a string
+print("\n")
+s0 = input("Enter any text below that you want to count Occurrences :- \n")
+s = "".join(s0.split(" "))
+
+x = set(s)
+x = list(x)
+x.sort(reverse=True)
+
+print("\n\n")
+print("In your submission, There are",len(s0.split(".")),"sentences and",len(s0.split(" ")),"words.\n")
+print("There are",len(x),"unique characters in your String.")
+print("\n\n")
+
+for a in x:
+	c = s.count(a)
+	print(a,"---",c)
+
+	
+print("\nPyramid :-")	
+b=0
+z=("")
+while (b < len(x)):
+	y = x[b]
+	z = z+y
+	print(z)
+	b += 1
